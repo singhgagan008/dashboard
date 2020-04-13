5439ffea2d67bec0153c69fbcb712839c1b8a8a0
Gurdip Singh <singh.gurdip24@gmail.com>
2020-04-13 14:35:28 -0400

    added colors

--- src/components/CardTable.js
+++ src/components/CardTable.js
@@ -8,13 +8,13 @@ const CardTable = (props) => {
     return(
         <div className='card-table'>
             <Card title="Total Cases" className="maincounter-wrap">
-                <h1>{props.totalCases}</h1>
+                <h1 style ={{color:'blue'}}>{props.totalCases}</h1>
             </Card>
             <Card title="Total Recovered" className="maincounter-wrap">
-                <h1>{props.totalRecovered}</h1>
+                <h1 style ={{color:'green'}}>{props.totalRecovered}</h1>
             </Card>
             <Card title="Total Deaths" className="maincounter-wrap">
-                <h1>{props.totalDeaths}</h1>
+                <h1 style ={{color:'red'}}>{props.totalDeaths}</h1>
             </Card>
         </div>
     );
