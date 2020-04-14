7e833fbfb3a3b804835f96125062fcd0e1bbe8ef
Gurdip Singh <singh.gurdip24@gmail.com>
2020-04-13 20:17:06 -0400

    added sort functionality

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,35 +4,76 @@ export const COLUMN_NAME = [
     {
       title: "Country",
       dataIndex: "country",
-      width: 150
+      width: 150,
+      sorter: {
+        compare: 
+        function(a, b) {
+          if (a.country > b.country) {
+            return 1;
+          }
+          if (a.country < b.country) {
+            return -1;
+          }
+          return 0;
+        }
+        ,
+        multiple: 1,
+      },
     },
     {
       title: "Total Confirmed",
       dataIndex: "totalConfirmed",
-      width: 150
+      width: 150,
+      sorter: {
+        compare: (a, b) => a.totalConfirmed - b.totalConfirmed,
+        multiple: 2,
+      },
     },
     {
       title: "New Confirmed",
       dataIndex: "newConfirmed",
-      width: 150
+      width: 150,
+      sorter: {
+        compare: (a, b) => a.newConfirmed - b.newConfirmed,
+        multiple: 3,
+      },
     },
     {
       title: "Total Deaths",
-      dataIndex: "totalDeaths"
+      dataIndex: "totalDeaths",
+      width: 150,
+      sorter: {
+        compare: (a, b) => a.totalDeaths - b.totalDeaths,
+        multiple: 4,
+      },
     },
     {
       title: "New Deaths",
       dataIndex: "newDeaths",
-      width: 150
+      width: 150,
+      className:"deaths-column",
+      sorter: {
+        compare: (a, b) => a.newDeaths - b.newDeaths,
+        multiple: 5,
+      },
     },
     {
       title: "Total Recovered",
       dataIndex: "totalRecovered",
-      width: 150
+      width: 150,
+      sorter: {
+        compare: (a, b) => a.totalRecovered - b.totalRecovered,
+        multiple: 6,
+      },
     },
     {
       title: "New Recovered",
-      dataIndex: "newRecovered"
+      dataIndex: "newRecovered",
+      width: 150,
+      sorter: {
+        compare: (a, b) => a.newRecovered - b.newRecovered,
+        multiple: 7,
+      },
     }
   ];
 
