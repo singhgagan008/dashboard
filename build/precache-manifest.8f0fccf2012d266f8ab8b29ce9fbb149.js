f04be07dfffa8ddd5f0e9587dead626600a366eb
Gurdip Singh <singh.gurdip24@gmail.com>
2020-06-08 19:57:30 -0400

    Build push

---
+++ build/precache-manifest.8f0fccf2012d266f8ab8b29ce9fbb149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbec9eef2b6cd0e8ab3cd3379ba52eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09e70db1ad226204a0c7",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "526a5d760012ca8c3edf",
+    "url": "/static/css/main.75cfc4cf.chunk.css"
+  },
+  {
+    "revision": "09e70db1ad226204a0c7",
+    "url": "/static/js/2.26bf3963.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.26bf3963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526a5d760012ca8c3edf",
+    "url": "/static/js/main.2c52a88f.chunk.js"
+  },
+  {
+    "revision": "bc99a48c8cbd57fdc428",
+    "url": "/static/js/runtime-main.61b2e5df.js"
+  }
+]);
